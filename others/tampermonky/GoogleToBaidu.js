1a2b666bff80ef1d96c0a8194e857825625a3342
unknown <zhoukq@FT.CNTAIPING.AD>
2020-01-15 16:55:55 +0800

    add new script

---
+++ others/tampermonky/GoogleToBaidu.js
@@ -0,0 +1,247 @@
+
+// ==UserScript==
+// @name         google伪装百度
+// @namespace
+// @version      0.1
+// @description  伪装百度！
+// @author       zkq
+// @match        *://www.google.is/*
+// @match        *://www.google.dk/*
+// @match        *://www.google.no/*
+// @match        *://www.google.se/*
+// @match        *://www.google.fi/*
+// @match        *://www.google.ee/*
+// @match        *://www.google.lv/*
+// @match        *://www.google.lt/*
+// @match        *://www.google.ie/*
+// @match        *://www.google.co.uk/*
+// @match        *://www.google.gg/*
+// @match        *://www.google.je/*
+// @match        *://www.google.im/*
+// @match        *://www.google.fr/*
+// @match        *://www.google.nl/*
+// @match        *://www.google.be/*
+// @match        *://www.google.lu/*
+// @match        *://www.google.de/*
+// @match        *://www.google.at/*
+// @match        *://www.google.ch/*
+// @match        *://www.google.li/*
+// @match        *://www.google.pt/*
+// @match        *://www.google.es/*
+// @match        *://www.google.com.gi/*
+// @match        *://www.google.ad/*
+// @match        *://www.google.it/*
+// @match        *://www.google.com.mt/*
+// @match        *://www.google.sm/*
+// @match        *://www.google.gr/*
+// @match        *://www.google.ru/*
+// @match        *://www.google.com.by/*
+// @match        *://www.google.com.ua/*
+// @match        *://www.google.pl/*
+// @match        *://www.google.cz/*
+// @match        *://www.google.sk/*
+// @match        *://www.google.hu/*
+// @match        *://www.google.si/*
+// @match        *://www.google.hr/*
+// @match        *://www.google.ba/*
+// @match        *://www.google.me/*
+// @match        *://www.google.rs/*
+// @match        *://www.google.mk/*
+// @match        *://www.google.bg/*
+// @match        *://www.google.ro/*
+// @match        *://www.google.md/*
+// @match        *://www.google.com.hk/*
+// @match        *://www.google.mn/*
+// @match        *://www.google.co.kr/*
+// @match        *://www.google.co.jp/*
+// @match        *://www.google.com.vn/*
+// @match        *://www.google.la/*
+// @match        *://www.google.com.kh/*
+// @match        *://www.google.co.th/*
+// @match        *://www.google.com.my/*
+// @match        *://www.google.com.sg/*
+// @match        *://www.google.com.bn/*
+// @match        *://www.google.com.ph/*
+// @match        *://www.google.co.id/*
+// @match        *://www.google.tp/*
+// @match        *://www.google.kz/*
+// @match        *://www.google.kg/*
+// @match        *://www.google.com.tj/*
+// @match        *://www.google.co.uz/*
+// @match        *://www.google.tm/*
+// @match        *://www.google.com.af/*
+// @match        *://www.google.com.pk/*
+// @match        *://www.google.com.np/*
+// @match        *://www.google.co.in/*
+// @match        *://www.google.com.bd/*
+// @match        *://www.google.lk/*
+// @match        *://www.google.mv/*
+// @match        *://www.google.com.kw/*
+// @match        *://www.google.com.sa/*
+// @match        *://www.google.com.bh/*
+// @match        *://www.google.ae/*
+// @match        *://www.google.com.om/*
+// @match        *://www.google.jo/*
+// @match        *://www.google.co.il/*
+// @match        *://www.google.com.lb/*
+// @match        *://www.google.com.tr/*
+// @match        *://www.google.az/*
+// @match        *://www.google.am/*
+// @match        *://www.google.co.ls/*
+// @match        *://www.google.com.eg/*
+// @match        *://www.google.com.ly/*
+// @match        *://www.google.dz/*
+// @match        *://www.google.co.ma/*
+// @match        *://www.google.sn/*
+// @match        *://www.google.gm/*
+// @match        *://www.google.ml/*
+// @match        *://www.google.bf/*
+// @match        *://www.google.com.sl/*
+// @match        *://www.google.ci/*
+// @match        *://www.google.com.gh/*
+// @match        *://www.google.tg/*
+// @match        *://www.google.bj/*
+// @match        *://www.google.ne/*
+// @match        *://www.google.com.ng/*
+// @match        *://www.google.sh/*
+// @match        *://www.google.cm/*
+// @match        *://www.google.td/*
+// @match        *://www.google.cf/*
+// @match        *://www.google.ga/*
+// @match        *://www.google.cg/*
+// @match        *://www.google.cd/*
+// @match        *://www.google.it.ao/*
+// @match        *://www.google.com.et/*
+// @match        *://www.google.dj/*
+// @match        *://www.google.co.ke/*
+// @match        *://www.google.co.ug/*
+// @match        *://www.google.co.tz/*
+// @match        *://www.google.rw/*
+// @match        *://www.google.bi/*
+// @match        *://www.google.mw/*
+// @match        *://www.google.co.mz/*
+// @match        *://www.google.mg/*
+// @match        *://www.google.sc/*
+// @match        *://www.google.mu/*
+// @match        *://www.google.co.zm/*
+// @match        *://www.google.co.zw/*
+// @match        *://www.google.co.bw/*
+// @match        *://www.google.com.na/*
+// @match        *://www.google.co.za/*
+// @match        *://www.google.com.au/*
+// @match        *://www.google.com.nf/*
+// @match        *://www.google.co.nz/*
+// @match        *://www.google.com.sb/*
+// @match        *://www.google.com.fj/*
+// @match        *://www.google.fm/*
+// @match        *://www.google.ki/*
+// @match        *://www.google.nr/*
+// @match        *://www.google.tk/*
+// @match        *://www.google.ws/*
+// @match        *://www.google.as/*
+// @match        *://www.google.to/*
+// @match        *://www.google.nu/*
+// @match        *://www.google.co.ck/*
+// @match        *://www.google.com.do/*
+// @match        *://www.google.tt/*
+// @match        *://www.google.com.co/*
+// @match        *://www.google.com.ec/*
+// @match        *://www.google.co.ve/*
+// @match        *://www.google.gy/*
+// @match        *://www.google.com.pe/*
+// @match        *://www.google.com.bo/*
+// @match        *://www.google.com.py/*
+// @match        *://www.google.com.br/*
+// @match        *://www.google.com.uy/*
+// @match        *://www.google.com.ar/*
+// @match        *://www.google.cl/*
+// @match        *://www.google.gl/*
+// @match        *://www.google.ca/*
+// @match        *://www.google.com/*
+// @match        *://www.google.com.mx/*
+// @match        *://www.google.com.gt/*
+// @match        *://www.google.com.bz/*
+// @match        *://www.google.com.sv/*
+// @match        *://www.google.hn/*
+// @match        *://www.google.com.ni/*
+// @match        *://www.google.co.cr/*
+// @match        *://www.google.com.pa/*
+// @match        *://www.google.bs/*
+// @match        *://www.google.com.cu/*
+// @match        *://www.google.com.jm/*
+// @match        *://www.google.ht/*
+// @grant        GM_log
+// @run-at document-start
+// ==/UserScript==
+
+var $ = document.querySelector.bind(document);
+var $$ = document.querySelectorAll.bind(document);
+
+
+
+(function () {
+    'use strict';
+
+    var interval = 10;
+    var status = 'loading';
+    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    link.type = 'image/x-icon';
+    link.rel = 'shortcut icon';
+    link.href = 'https://www.baidu.com/favicon.ico';
+    document.getElementsByTagName('head')[0].appendChild(link);
+    document.addEventListener('DOMContentLoaded', function () { status = 'complete' });
+
+    function run() {
+        logoToBaidu();
+        navTab();
+    }
+
+    function navTab() {
+        let navTabSpans = $$(".csb");
+
+        if (!navTabSpans || navTabSpans.length == 0) {
+            if (status != 'complete') {
+                setTimeout(navTab, 10);
+                return;
+            } else {
+                console.log('加载完成，退出..');
+            }
+        }
+        for (var i = 0; i < navTabSpans.length; i++) {
+            var naviImageUrl = "https://ss1.bdstatic.com/5eN1bjq8AAUYm2zgoY3K/r/www/cache/static/protocol/https/global/img/icons_5859e57.png";
+            navTabSpans[i].style.width = "22px";
+            if (i === 0) { //开始的大G
+                navTabSpans[i].style.background = 'url("' + naviImageUrl + '") no-repeat 0px 0px';
+            }
+            else if (navTabSpans[i].classList.contains("ch")) {// 变灰色的导航页
+                navTabSpans[i].style.background = i % 2 == 1 ? 'url("' + naviImageUrl + '") no-repeat -144px -288px' : 'url("' + naviImageUrl + '") no-repeat -144px -282px'; //让页面底部的百度脚丫子错落有致,感谢Raka-loah
+            }
+            else { //当前导航页
+                navTabSpans[i].style.background = 'url("' + naviImageUrl + '") no-repeat -96px -288px';
+            }
+        }
+    }
+
+    function logoToBaidu() {
+        let logo = $('#logo img');
+        if (!logo) {
+            logo = $('.logo img');
+        }
+
+
+        if (logo) {
+            logo.src = 'https://ss0.bdstatic.com/5aV1bjqh_Q23odCf/static/superman/img/logo_top_86d58ae1.png';
+            return;
+        }
+        console.log(status);
+        if (status == 'loading') {
+            setTimeout(logoToBaidu, interval);
+        }
+
+    }
+
+
+
+    run();
+    // Your code here...
+})();
